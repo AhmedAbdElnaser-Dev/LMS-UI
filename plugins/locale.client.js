224c1b499814e176f74b340930831b4a7c447b7f
Ahmed Abd Elnaser <102987038+Eng-0marleves@users.noreply.github.com>
2025-02-09 06:18:03 +0200

    [fix] depoy error

--- plugins/locale.client.js
+++ plugins/locale.client.js
@@ -1,9 +1,10 @@
 export default defineNuxtPlugin((nuxtApp) => {
-	const savedLocale = localStorage.getItem("locale") || "en";
+	if (process.client) {
+		const savedLocale = localStorage.getItem("locale") || "en";
+		const i18n = nuxtApp.$i18n;
 
-	const i18n = nuxtApp.$i18n;
-
-	if (i18n && i18n.locale._value !== savedLocale) {
-		i18n.setLocale(savedLocale);
+		if (i18n && i18n.locale._value !== savedLocale) {
+			i18n.setLocale(savedLocale);
+		}
 	}
 });
